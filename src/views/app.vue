48170b0e001a94ba85f0c2fda87cf0a689663927
zhuowenli <zhuowenligg@gmail.com>
2019-12-24 16:55:24 +0800

    :rotating_light: 添加 eslint-config-gaoding-vue 配置

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <RouterView />
     </div>
 </template>
 
@@ -14,6 +14,6 @@ export default {
     mounted() {
     },
     methods: {
-    }
+    },
 };
 </script>
